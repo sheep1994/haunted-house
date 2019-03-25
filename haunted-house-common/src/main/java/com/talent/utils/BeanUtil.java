e8a59feaa9d146be37996d8ea3e1297020da7f3e
郭兵 <m13571829170@163.com>
2019-03-25 13:18:26 +0800

    项目构建

---
+++ haunted-house-common/src/main/java/com/talent/utils/BeanUtil.java
@@ -0,0 +1,11 @@
+package com.talent.utils;
+
+/**
+ * @author guobing
+ * @Title: BeanUtil
+ * @ProjectName haunted-house
+ * @Description: TODO
+ * @date 2019/3/2511:53 AM
+ */
+public class BeanUtil {
+}
